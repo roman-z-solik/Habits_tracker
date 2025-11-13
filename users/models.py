8f8804fa6565a05fe6d3377dddbc542966882b06
Roman Z <roman-z@inbox.ru>
2025-11-13 11:58:46 +0500

    add User and Habit models with basic structure

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    telegram_chat_id = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Telegram Chat ID'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
